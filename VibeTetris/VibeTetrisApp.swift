a2ac36ac190012bc6344a5a69b43dc9aa96c9bef
Maxim Shevelev <max.shevelev@web.de>
2026-05-21 10:10:03 +0200

    Initial Commit

---
+++ VibeTetris/VibeTetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeTetrisApp.swift
+//  VibeTetris
+//
+//  Created by Maxim Shevelev on 21.05.26.
+//
+
+import SwiftUI
+
+@main
+struct VibeTetrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VibeTetrisTests/VibeTetrisTests.swift
@@ -0,0 +1,16 @@
+//
+//  VibeTetrisTests.swift
+//  VibeTetrisTests
+//
+//  Created by Maxim Shevelev on 21.05.26.
+//
+
+import Testing
+
+struct VibeTetrisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
