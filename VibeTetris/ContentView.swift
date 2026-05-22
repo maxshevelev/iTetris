ee05aabd5adb560414be01f14e5f4d5942c439f1
Maxim Shevelev <max.shevelev@web.de>
2026-05-22 09:58:46 +0200

    Delay flash until hard drop animation completes

--- VibeTetris/ContentView.swift
+++ VibeTetris/ContentView.swift
@@ -78,13 +78,12 @@ struct ContentView: View {
                 withAnimation(.easeIn(duration: viewModel.hardDropAnimDuration)) {
                     hardDropProgress = 1.0
                 }
-            }
-        }
-        .onChange(of: viewModel.hardDropTrigger) {
-            hardDropFlash = true
-            Task {
-                try? await Task.sleep(for: .milliseconds(80))
-                hardDropFlash = false
+                Task {
+                    try? await Task.sleep(for: .seconds(viewModel.hardDropAnimDuration + 0.05))
+                    hardDropFlash = true
+                    try? await Task.sleep(for: .milliseconds(80))
+                    hardDropFlash = false
+                }
             }
         }
     }
